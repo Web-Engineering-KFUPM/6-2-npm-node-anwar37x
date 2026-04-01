7ac3f28aa10654084bc431e98c9352da8e76e0b9
anwar37x <s202242640@kfupm.edu.sa>
2026-04-01 12:39:23 +0300

    1

--- calculator.js
+++ calculator.js
@@ -212,4 +212,38 @@ After completing all TODOs, test your calculator:
   Expected output: Invalid operation. Use: add, subtract, multiply, or divide
 
 */
+import { add, subtract, multiply, divide } from "./utils/operations.js";
+import { parseNumbers, isValidOperation } from "./utils/parser.js";
+import _ from "lodash";
+
+// TODO 2
+const operation = process.argv[2];
+const numbers = process.argv.slice(3);
+
+// TODO 3
+if (!isValidOperation(operation)) {
+  console.log("Invalid operation. Use: add, subtract, multiply, or divide");
+  process.exit();
+}
+
+const nums = parseNumbers(numbers);
+
+let result;
+
+switch (operation) {
+  case "add":
+    result = add(nums);
+    break;
+  case "subtract":
+    result = subtract(nums);
+    break;
+  case "multiply":
+    result = multiply(nums);
+    break;
+  case "divide":
+    result = divide(nums);
+    break;
+}
+
+console.log(`Result: ${result}`);
 
---
+++ utils/operations.js
@@ -0,0 +1,15 @@
+export function add(numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0);
+}
+
+export function subtract(numbers) {
+  return numbers.slice(1).reduce((res, num) => res - num, numbers[0]);
+}
+
+export function multiply(numbers) {
+  return numbers.reduce((res, num) => res * num, 1);
+}
+
+export function divide(numbers) {
+  return numbers.slice(1).reduce((res, num) => res / num, numbers[0]);
+}
